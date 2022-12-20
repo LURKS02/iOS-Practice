2b079028fecaf49077001ce4b5b5f4b3b7b78075
LURKS02 <thecrooks2@gmail.com>
2022-12-21 03:24:06 +0900

    foreach

---
+++ MyPlayground.playground/Pages/foreach.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var myArray : [Int] =
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for item in myArray {
+    print("item : \(item)")
+}
+
+for item in myArray where item > 5 {
+    print("5보다 큰수 : \(item)")
+}
+
+for item in myArray where item % 2 == 0 {
+    print("짝수 : \(item)")
+}
