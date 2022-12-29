79277d7cef04560637efb51e44a071ad7ef5046f
LURKS02 <thecrooks2@gmail.com>
2022-12-30 07:01:53 +0900

    property added

--- MyPlayground.playground/Pages/property.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/property.xcplaygroundpage/Contents.swift
@@ -9,3 +9,29 @@ var myAge = 0 {
 
 print("myAge : \(myAge)")
 myAge = 20
+
+class Person {
+
+  var name: String = "KIM"
+
+  var alias: String {
+    get {           //getter : 어떤 저장 프로퍼티의 값을 연산해서 return할 것인지
+      return name
+    }
+    set(name) {     //setter : 파라미터로 받은 값을 어떤 저장 프로퍼티에 어떻게 설정할 것인지
+      self.name = name
+    }
+  }
+}
+let kim: Person = .init()
+kim.alias = "Jake"  //set에 접근
+print(kim.alias)    //get에 접근
+
+
+class Person {
+  static let name: String = "KIM"
+  static var alias: String {
+    return name
+  }
+}
+print(Person.name)
