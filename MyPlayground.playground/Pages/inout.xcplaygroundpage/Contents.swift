27fbfeff22531a554798b0ee8af2114c13d4918f
LURKS02 <thecrooks2@gmail.com>
2023-01-03 22:00:59 +0900

    inout added

--- MyPlayground.playground/Pages/inout.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/inout.xcplaygroundpage/Contents.swift
@@ -11,3 +11,14 @@ func say2(_ name: inout String) {
 var name = "KIM"
 
 say2(&name)
+
+func swap(_ aVal: inout Int, _ bVal: inout Int) {
+    let tempVal = aVal
+    aVal = bVal
+    bVal = tempVal
+}
+
+var firstInt = 100
+var secondInt = 200
+swap(&firstInt, &secondInt)
+print("firstInt : \(firstInt), secondInt : \(secondInt)")
