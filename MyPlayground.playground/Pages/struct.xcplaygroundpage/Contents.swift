e9dc5f7399dedd67754a7e13a53bf958ad6550f4
LURKS02 <thecrooks2@gmail.com>
2023-01-07 19:08:56 +0900

    mutating

--- MyPlayground.playground/Pages/struct.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/struct.xcplaygroundpage/Contents.swift
@@ -11,3 +11,23 @@ MIN.name = "MIN"
 //값 복사
 print("CHOI.name = \(CHOI.name)")
 print("MIN.name = \(MIN.name)")
+
+struct Player {
+    var name: String
+    
+    func nameChange() {
+        name = "KIM"
+    }
+}  //오류 : self is immutable
+
+struct Player {
+    var name: String
+    
+    mutating func nameChange() {
+        name = "KIM"
+    }
+}
+
+var player1 = Player(name: "LEE")
+player1.nameChange()
+print(player1.name)
