a16971991f7400e0b8b006ffc40b36032e038631
LURKS02 <thecrooks2@gmail.com>
2023-01-09 12:39:14 +0900

    pigma test

--- LearningApp/ContentView.swift
+++ LearningApp/ContentView.swift
@@ -14,17 +14,39 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text("잠깐! 펫니티가 처음이신가요?")
-                    .font(.title)
-                    .padding()
-                Text("간단한 회원가입으로 펫니티의 모든 서비스를 이용하실 수 있어요!")
+                Text("잠깐!\n펫니티가 처음이신가요?")
+                .fontWeight(.bold)
+                .font(.title)
+                .lineSpacing(5)
+                
+                Spacer().frame(height:30)
+                
+                Text("간단한 회원가입으로\n펫니티의 모든 서비스를 이용하실 수 있어요! ")
+                .fontWeight(.medium)
+                .font(.subheadline)
+                .lineSpacing(2)
+                
+                Spacer().frame(height: 270)
                 
                 NavigationLink (
                 destination: NextView(),
                 label: {
-                    Text("navigate")
+                    Text("회원가입 하기")
+                    .fontWeight(.semibold)
+                    .font(.title2)
+                    .lineSpacing(25)
+                    .padding(.horizontal, 30)
+                    .padding(.vertical, 20)
+                    .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+                    .cornerRadius(40)
                 })
                 
+                Spacer().frame(height: 15)
+                
+                Text("기존 아이디로 로그인하기")
+                .underline()
+                .fontWeight(.bold)
+                .font(.footnote)
                 
             }
         }
@@ -33,7 +55,95 @@ struct ContentView: View {
 
 struct NextView: View {
     var body: some View {
-        Text("두번째 화면")
+        HStack(spacing: 0) {
+            Text("이름")
+            .font(.body)
+            .lineSpacing(22)
+            .padding(.trailing, 53)
+
+            Text("이름을 입력하세요.")
+            .font(.body)
+            .lineSpacing(22)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 7)
+        .padding(.leading, 8)
+        .padding(.trailing, 14)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .frame(width: 343)
+        
+        HStack(spacing: 0) {
+            Text("이메일")
+            .font(.body)
+            .lineSpacing(22)
+            .padding(.trailing, 37)
+
+            Text("이메일을 입력하세요.")
+            .font(.body)
+            .lineSpacing(22)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 7)
+        .padding(.leading, 8)
+        .padding(.trailing, 14)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .frame(width: 343)
+        
+        Rectangle()
+        .frame(width: 370, height: 1)
+        
+        HStack(spacing: 0) {
+            Text("비밀번호")
+            .font(.body)
+            .lineSpacing(22)
+            .padding(.trailing, 23)
+
+            Text("비밀번호를 입력하세요.")
+            .font(.body)
+            .lineSpacing(22)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 7)
+        .padding(.leading, 8)
+        .padding(.trailing, 14)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .frame(width: 343)
+        
+        HStack(spacing: 0) {
+            Text("비밀번호 재입력")
+            .font(.body)
+            .lineSpacing(22)
+            .padding(.trailing, 23)
+
+            Text("비밀번호를 다시 입력하세요.")
+            .font(.body)
+            .lineSpacing(22)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 7)
+        .padding(.leading, 8)
+        .padding(.trailing, 14)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .frame(width: 343)
+        
+        Rectangle()
+        .frame(width: 370, height: 1)
+        
+        Spacer().frame(height: 200)
+        
+        Text("회원가입")
+        .fontWeight(.semibold)
+        .font(.title3)
+        .lineSpacing(25)
+        .padding(.horizontal, 22)
+        .padding(.vertical, 17)
+        .frame(width: 296, height: 53)
+        .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+        .cornerRadius(40)
     }
 }
 
@@ -42,3 +152,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
