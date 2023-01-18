ca59036ee170282f0eb3b6f6ec41c75ac65ca26c
LURKS02 <thecrooks2@gmail.com>
2023-01-18 18:34:12 +0900

    main view

--- LearningApp/MainView.swift
+++ LearningApp/MainView.swift
@@ -10,90 +10,93 @@ import SwiftUI
 struct MainView: View {
     var body: some View {
         
-        VStack {
-            Image("dogImg")
-                .resizable()
-                .frame(width: 393, height: 370)
-            
-            Spacer().frame(height:30)
-            
-            HStack (alignment: .bottom) {
-                Text("쫑이")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .padding(.trailing, 7)
-
-                Text("3살")
-                .fontWeight(.semibold)
-                .font(.headline)
-                .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
-
-                Text("10kg")
-                .fontWeight(.semibold)
-                .font(.headline)
-                .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
-
-                Spacer()
+        ScrollView(.vertical, showsIndicators: false, content: {
+            VStack {
+                Image("dogImg")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 393, height: 370)
                 
-                Text("19.10.11")
-                .fontWeight(.semibold)
-                .font(.headline)
-                .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
-            }
-            .padding(.horizontal, 30)
-            
-            Spacer().frame(height: 25)
-            
-            HStack {
-                VStack (alignment: .leading) {
-                    Text("웰시코기")
-                        .fontWeight(.semibold)
-                        .font(.title3)
-                        .foregroundColor(Color(red: 0.23, green: 0.23, blue: 0.23))
+                Spacer().frame(height:30)
+                
+                HStack (alignment: .bottom) {
+                    Text("쫑이")
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .padding(.trailing, 7)
+
+                    Text("3살")
+                    .fontWeight(.semibold)
+                    .font(.headline)
+                    .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
+
+                    Text("10kg")
+                    .fontWeight(.semibold)
+                    .font(.headline)
+                    .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
+
+                    Spacer()
                     
-                    Spacer().frame(height: 3)
-                    
-                    Text("서울특별시 마포구 망원동")
-                        .fontWeight(.semibold)
-                        .font(.title3)
-                        .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
+                    Text("19.10.11")
+                    .fontWeight(.semibold)
+                    .font(.headline)
+                    .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
                 }
                 .padding(.horizontal, 30)
                 
-                Spacer()
+                Spacer().frame(height: 25)
                 
-            }
-            
-            Spacer().frame(height: 40)
-            
-            HStack {
-                Text("오늘의 산책")
-                .fontWeight(.semibold)
-                .font(.title3)
-                .lineSpacing(22)
+                HStack {
+                    VStack (alignment: .leading) {
+                        Text("웰시코기")
+                            .fontWeight(.semibold)
+                            .font(.title3)
+                            .foregroundColor(Color(red: 0.23, green: 0.23, blue: 0.23))
+                        
+                        Spacer().frame(height: 3)
+                        
+                        Text("서울특별시 마포구 망원동")
+                            .fontWeight(.semibold)
+                            .font(.title3)
+                            .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
+                    }
+                    .padding(.horizontal, 30)
+                    
+                    Spacer()
+                    
+                }
+                
+                Spacer().frame(height: 40)
+                
+                HStack {
+                    Text("오늘의 산책")
+                    .fontWeight(.semibold)
+                    .font(.title3)
+                    .lineSpacing(22)
 
-                Rectangle()
-                .frame(width: 233, height: 1)
-                .foregroundColor(Color(red: 0.82, green: 0.82, blue: 0.82))
+                    Rectangle()
+                    .frame(width: 233, height: 1)
+                    .foregroundColor(Color(red: 0.82, green: 0.82, blue: 0.82))
+                }
+                
+                HStack {
+                    Text("1.3 km ")
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .foregroundColor(Color(red: 0.37, green: 0.68, blue: 0.68)) +
+                    Text("를 걸었어요!")
+                    .fontWeight(.semibold)
+                    .font(.headline)
+                    .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
+                    Spacer()
+                }
+                .padding(.horizontal, 30)
+                
+                Image("mapImg1")
+                    .resizable()
+                    .frame(width: 393, height: 350)
             }
-            
-            HStack {
-                Text("1.3 km ")
-                .fontWeight(.black)
-                .font(.largeTitle)
-                .foregroundColor(Color(red: 0.37, green: 0.68, blue: 0.68)) +
-                Text("를 걸었어요!")
-                .fontWeight(.semibold)
-                .font(.headline)
-                .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
-                Spacer()
-            }
-            .padding(.horizontal, 30)
-            
-            Image("mapImg1")
-                .resizable()
-                .frame(width: 393, height: 350)
-        }
+        })
     }
 }
 
