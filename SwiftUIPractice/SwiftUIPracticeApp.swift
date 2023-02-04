60c99f6857e180ee6d7b9b28984060d8886a82ba
LURKS02 <thecrooks2@gmail.com>
2023-02-05 06:58:47 +0900

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by 디해 on 2023/02/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
